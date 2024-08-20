79c384bbc00374cf7c3838ab0525d952d345b726
l30039587 <liuxin407@huawei.com>
2024-08-20 11:22:32 +0800

    字母异位词分组

--- leetcode/49-MEDIUM-字母异位词分组/lx.ts
+++ leetcode/49-MEDIUM-字母异位词分组/lx.ts
@@ -1,3 +1,20 @@
 export function groupAnagrams(strs: string[]): string[][] {
-    
+  const myTypedMap: Map<string, string[]> = new Map()
+
+  for (let i = 0; i < strs.length; i++) {
+    const key = sortStr(strs[i])
+    if (myTypedMap.has(key)) {
+      const value = myTypedMap.get(key) || []
+      value.push(strs[i])
+    }
+    else {
+      myTypedMap.set(key, [strs[i]])
+    }
+  }
+
+  return Array.from(myTypedMap.values())
 };
+function sortStr(str: string): string {
+  const s = str.split('').sort().join('')
+  return s
+}
