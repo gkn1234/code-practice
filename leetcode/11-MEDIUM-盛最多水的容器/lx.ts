8f8c651227a01c88999c31515a6e5accff6f6d85
l30039587 <liuxin407@huawei.com>
2024-08-22 18:23:30 +0800

    盛最多水的容器

--- leetcode/11-MEDIUM-盛最多水的容器/lx.ts
+++ leetcode/11-MEDIUM-盛最多水的容器/lx.ts
@@ -1,3 +1,11 @@
 export function maxArea(height: number[]): number {
-    
-};
+  let maxArea = 0
+  for (let i = 0; i < height.length; i++) {
+    for (let j = i + 1; j < height.length; j++) {
+      const area = Math.min(height[i], height[j]) * (j - i)
+      maxArea = Math.max(maxArea, area)
+    }
+  }
+  return maxArea
+}
+// 暴力解法，超时，需要知道如何使用双指针进行解答
