5e199bc66d36b392e01733e1d6e2da08a3db3d8f
l30039587 <liuxin407@huawei.com>
2024-08-23 19:13:02 +0800

    盛最多水的容器补充双指针解答

--- leetcode/11-MEDIUM-盛最多水的容器/lx.ts
+++ leetcode/11-MEDIUM-盛最多水的容器/lx.ts
@@ -1,11 +1,31 @@
+// export function maxArea(height: number[]): number {
+//   let maxArea = 0
+//   for (let i = 0; i < height.length; i++) {
+//     for (let j = i + 1; j < height.length; j++) {
+//       const area = Math.min(height[i], height[j]) * (j - i)
+//       maxArea = Math.max(maxArea, area)
+//     }
+//   }
+//   return maxArea
+// }
+// 暴力解法，超时，需要知道如何使用双指针进行解答
+
+// 以下为双指针解法
 export function maxArea(height: number[]): number {
   let maxArea = 0
+  let left = 0
+  let right = height.length - 1
+
   for (let i = 0; i < height.length; i++) {
-    for (let j = i + 1; j < height.length; j++) {
-      const area = Math.min(height[i], height[j]) * (j - i)
-      maxArea = Math.max(maxArea, area)
+    const area = Math.min(height[left], height[right]) * (right - left)
+    if (height[left] < height[right]) {
+      left++
     }
+    else {
+      right--
+    }
+    maxArea = Math.max(maxArea, area)
   }
+
   return maxArea
 }
-// 暴力解法，超时，需要知道如何使用双指针进行解答
